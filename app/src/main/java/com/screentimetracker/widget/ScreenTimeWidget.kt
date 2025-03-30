15838e114f015543f1281b6fc583029253c13d55
Pankajknew <pankaj31ec@gmail.com>
2025-03-30 21:07:28 +0530

    Create ScreenTimeWidget.kt

---
+++ app/src/main/java/com/screentimetracker/widget/ScreenTimeWidget.kt
@@ -0,0 +1,11 @@
+package com.screentimetracker.widget
+
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.widget.RemoteViews
+
+class ScreenTimeWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // Widget update logic
+    }
+}
